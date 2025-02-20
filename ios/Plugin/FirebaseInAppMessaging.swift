8f05c89b1664b1e1ef98bb0d48231d901818dd13
Yigit Serin <yigit.serin@ihale.com.tr>
2025-02-20 15:49:44 +0300

    dismissType fixed. Click problem fixed.

--- ios/Plugin/FirebaseInAppMessaging.swift
+++ ios/Plugin/FirebaseInAppMessaging.swift
@@ -11,12 +11,35 @@ import FirebaseInAppMessaging
     }
     
     public func messageClicked(_ inAppMessage: InAppMessagingDisplayMessage) {
-        // ...
-        self.plugin?.notifyListeners("inAppMessagingMessageClicked", data: ["inAppMessage":inAppMessage], retainUntilConsumed: true)
+        var data: [String: Any] = ["inAppMessage": inAppMessage]
+        
+        var actionURL: URL? = nil
+        
+        switch inAppMessage {
+        case let cardMessage as InAppMessagingCardDisplay:
+            actionURL = cardMessage.primaryActionURL
+        case let bannerMessage as InAppMessagingBannerDisplay:
+            actionURL = bannerMessage.actionURL
+        case let imageMessage as InAppMessagingImageOnlyDisplay:
+            actionURL = imageMessage.actionURL
+        case let modalMessage as InAppMessagingModalDisplay:
+            actionURL = modalMessage.actionURL
+        default:
+            break
+        }
+        
+        if let url = actionURL {
+            data["actionURL"] = url.absoluteString
+            if UIApplication.shared.canOpenURL(url) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        }
+        
+        self.plugin?.notifyListeners("inAppMessagingMessageClicked", data: data, retainUntilConsumed: true)
     }
 
     public  func messageDismissed(_ inAppMessage: InAppMessagingDisplayMessage,
-                          dismissType: FIRInAppMessagingDismissType) {
+                                  dismissType: InAppMessagingDismissType) {
         // ...
         self.plugin?.notifyListeners("inAppMessagingMessageDismissed", data: ["inAppMessage":inAppMessage], retainUntilConsumed: true)
     }
